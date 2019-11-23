3d0976bb2b854894e21d933c0ad057e22ee4897c
Alex <alexmiddleton@outlook.com>
2019-11-23 12:13:18 -0500

    Adjusted margins on rootMargin to keep navbar in place all the way down the page.  Temporary fix.

--- src/components/Technology/Technology.js
+++ src/components/Technology/Technology.js
@@ -8,7 +8,7 @@ const Technology = () => {
             <div className="technology-container">
                 <h1>Programming</h1>
                 <hr style={{ width: '100% '}} />
-                <p><strong>Languages: </strong>JavaScript, Python</p>
+                <p><strong>Languages: </strong>JavaScript, Python, SQL</p>
                 <p><strong>Frontend: </strong>HTML, CSS, React, Redux, Gatsby</p>
                 <p><strong>Backend: </strong>Node.js, SQL Server, GraphQL, Express, Pandas</p>
                 <p><strong>Version control system: </strong>Git</p>
