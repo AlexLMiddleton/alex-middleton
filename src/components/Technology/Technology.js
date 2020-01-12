09492f1abc6ff208f2eeec906abb0c02d1e6f8a1
Alex <alexmiddleton@outlook.com>
2020-01-12 10:05:13 -0500

    Updated programming section.

--- src/components/Technology/Technology.js
+++ src/components/Technology/Technology.js
@@ -8,8 +8,8 @@ const Technology = () => {
             <div className="technology-container">
                 <h1>Programming</h1>
                 <hr style={{ width: '100% '}} />
-                <p><strong>Languages: </strong>JavaScript, Python, SQL</p>
-                <p><strong>Frontend: </strong>HTML, CSS, React, Redux, Gatsby</p>
+                <p><strong>Languages: </strong>HTML, CSS, JavaScript, Python, SQL</p>
+                <p><strong>Frontend: </strong>React, Redux, Gatsby</p>
                 <p><strong>Backend: </strong>Node.js, SQL Server, GraphQL, Express, Pandas</p>
                 <p><strong>Version control system: </strong>Git</p>
             </div>
