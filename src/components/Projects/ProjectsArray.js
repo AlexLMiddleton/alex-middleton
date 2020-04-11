b43dd11a5f8e53a71ae6cb97d0f2897ccf1b335a
Alex <alexmiddleton@outlook.com>
2020-04-11 18:47:57 -0400

    Added hair salon demo card.

--- src/components/Projects/ProjectsArray.js
+++ src/components/Projects/ProjectsArray.js
@@ -1,23 +1,32 @@
 const ProjectsArray = [
-    {
-        id: 1,
-        name: "Alex Middleton",
-        technologies: 'HTML, CSS, React',
-        description: 'This is a web portfolio built with React, HTML, and pure CSS.',
-        url: 'http://www.alex-middleton.com',
-        bgColor: 'navy',
-        completed: true
-    },
-    {
-        id: 2,
-        name: "Kristin Kee",
-        technologies: 'HTML, CSS, Gatsby, React, GraphQL',
-        description: 'This is an online resume for a marketer and project manager.',
-        url: 'http://www.kristinkee.com',
-        bgColor: 'blue',
-        completed: false
-    },
-]
+  {
+    id: 1,
+    name: "Alex Middleton",
+    technologies: "HTML, CSS, React",
+    description:
+      "This is a web portfolio built with React, HTML, and pure CSS.",
+    url: "http://www.alex-middleton.com",
+    bgColor: "navy",
+    completed: true
+  },
+  {
+    id: 2,
+    name: "Kristin Kee",
+    technologies: "HTML, CSS, Gatsby, React, GraphQL",
+    description: "This is an online resume for a marketer and project manager.",
+    url: "http://www.kristinkee.com",
+    bgColor: "blue",
+    completed: true
+  },
+  {
+    id: 3,
+    name: "Palmetto Salon",
+    technologies: "HTML, CSS, React",
+    description: "This is demo site I built for a local hair salon.",
+    url: "https://palmetto-salon-1.netlify.com/portfolio",
+    bgColor: "cornflowerblue",
+    completed: true
+  }
+];
 
-
-export default ProjectsArray
+export default ProjectsArray;
